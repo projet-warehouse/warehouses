486e3dabd3b3cd8f93ed924e5d96be1d86364e7a
BLACKHATTTTT <yaoboris01@icloud.com>
2021-12-17 12:58:54 +0000

    ajout

--- resources/views/visitor/customer/myaccount_wishlist.blade.php
+++ resources/views/visitor/customer/myaccount_wishlist.blade.php
@@ -28,12 +28,12 @@
 
    <div class="account__card border-bottom-0 border-left-0 border-top-0 border-right-0 h-100">
        <div class="account__card--header">
-          <h3 class="account__card--title"> Vos commandes</h3>          
+          <h3 class="account__card--title">Liste d'envies</h3>          
        </div>
 
        <div class="d-flex justify-content-around text-uppercase my-4 h6">
-        <div><a href="/myaccount/order/index" class="border border-bottom border-left-0 border-top-0 border-right-0 p-2 mb-6">Commandes en cours (03)</a></div>
-        <div><a href="/myaccount/order/close" class="border border-bottom border-left-0 border-top-0 border-right-0 p-2 concurrent-order">Commandes terminées (02)</a></div>
+        <div></div>
+        <div></div>
     </div>
 
         <div class="card mt-3 mb-3 mx-4 p-3 d-flex" style="">
@@ -45,15 +45,18 @@
                 <div class="card-body">
                   <h5 class="card-title"> Chaud Nouveau Réveil Rétro-éclairage Grand Affichage 
                     électronique LCD Réveil Numérique--Blanc</h5>
-                  <p class="card-text" style="max-height:200px; overflow:hidden">
-                    Commande N°01
-                    </p>
+                  
                 </div>
-                <p class="card-text"><small class="text-dark h6">Le 01 Novembre 2021</small></p>
-                <div class="bg-success px-3 orderStatus" id=""> LIVREE</div>
+
+                
               </div>
-                <div class="col-md-2 detail align-items-end" style="text-align: end; color:#f7941d;">
-                    <a href="" class="p-2">Détails</a>
+                <div class="col-md-2 detail align-items-end ">
+                <a href=""><div class="p-2 rounded text-white" style="text-align: center; font-weight: bold; background-color:orange;">Acheter</div></a>
+
+                <a href=""><div class="p-2 mt-3 rounded" style="text-align: center; background-color:white; color: orange;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+              <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+            </svg></i>Supprimer</div></a>
                 </div>
             </div>
         </div>
@@ -65,16 +68,19 @@
               </div>
               <div class="col-md-6">
                 <div class="card-body">
-                  <h5 class="card-title">Moulinex Bouilloire Soleil 1.7L Ivoire BY320A10</h5>
-                  <p class="card-text" style="max-height:200px; overflow:hidden">
-                    Commande N°04
-                    </p>
+                  <h5 class="card-title">TV SAMSUNG 4K </h5>
+                  
                 </div>
-                <p class="card-text"><small class="text-dark h6">Le 15 Décembre 2021</small></p>
-                <div class="bg-secondary px-3 orderStatus" id=""> ANNULEE</div>
+                <p class="card-text"></p>
+                
               </div>
-                <div class="col-md-2 detail align-items-end" style="text-align: end; color:#f7941d;">
-                    <a href="" class="p-2">Détails</a>
+                <div class="col-md-2 detail align-items-end">
+                <a href=""><div class="p-2 rounded text-white" style="text-align: center; font-weight: bold; background-color:orange;">Acheter</div></a>
+
+                <a href=""><div class="p-2 mt-3 rounded" style="text-align: center; background-color:white; color: orange;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+              <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+            </svg></i>Supprimer</div></a>
                 </div>
             </div>
         </div>
